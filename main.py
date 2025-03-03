9158f9735abaa49c4a342f474932c2c3274b3935
Patrick Ha <patrick.v.ha-1@ou.edu>
2025-03-03 13:37:59 -0600

    first function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def add(a, b):
-    return a + b
+import pandas as pd
+import random
 
+def print_random_movie(file_path='IMDB-Movie-Data.csv'):
+    # Load the CSV into a DataFrame
+    movies_df = pd.read_csv(file_path)
+
+    # Pick a random row
+    random_index = random.randint(0, len(movies_df) - 1)
+
+    # Get the title of the random movie
+    random_movie = movies_df.iloc[random_index]['Title']
+
+    # Print the result
+    print(f"Random Movie Pick: {random_movie}")
 
 if __name__ == "__main__":
-    print(f"2 + 3 = {add(2, 3)}")
+    print_random_movie()
