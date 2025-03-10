a77dec6ac961612eefe5d8a8d5b8bbe66c37c191
Markus Evan Means <markus.adam2014@gmail.com>
2025-03-10 14:01:55 -0500

    adding front end

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+@app.route('/get-random-movie')
+def get_random_movie():
+    movies_df = pd.read_csv('IMDB-Movie-Data.csv')
+    random_index = random.randint(0, len(movies_df) - 1)
+    random_movie = movies_df.iloc[random_index]['Title']
+    random_movie_rating = movies_df.iloc[random_index]['Rating']
+
+    return jsonify({
+        'title': random_movie,
+        'rating': random_movie_rating
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
