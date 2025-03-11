1d4eb35a5e8ae1bb65c062a2a4fe1dbf91367b9f
Markus Evan Means <markus.adam2014@gmail.com>
2025-03-11 11:48:14 -0500

    Add dynamic movie picker

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, jsonify
+import pandas as pd
+import random
 
 app = Flask(__name__)
 
+# Load movie data once at startup
+movies_df = pd.read_csv('IMDB-Movie-Data.csv')
+
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')  # Make sure 'index.html' is in a `/templates` folder
 
-@app.route('/<path:path>')
-def static_files(path):
-    return send_from_directory('.', path)
+@app.route('/random-movie', methods=['GET'])
+def get_random_movie():
+    random_index = random.randint(0, len(movies_df) - 1)
+    random_movie = movies_df.iloc[random_index]['Title']
+    random_movie_rating = movies_df.iloc[random_index]['Rating']
+
+    return jsonify({
+        "movie": random_movie,
+        "rating": random_movie_rating
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
