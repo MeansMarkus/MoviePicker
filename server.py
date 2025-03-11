71b620861e2d68e3ae954e3ac8a7d3e5c1368b26
Markus Evan Means <markus.adam2014@gmail.com>
2025-03-11 11:51:34 -0500

    revert

--- server.py
+++ server.py
@@ -1,26 +1,14 @@
-from flask import Flask, render_template, jsonify
-import pandas as pd
-import random
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-# Load movie data once at startup
-movies_df = pd.read_csv('IMDB-Movie-Data.csv')
-
 @app.route('/')
 def home():
-    return render_template('index.html')  # Make sure 'index.html' is in a `/templates` folder
+    return send_from_directory('.', 'index.html')
 
-@app.route('/random-movie', methods=['GET'])
-def get_random_movie():
-    random_index = random.randint(0, len(movies_df) - 1)
-    random_movie = movies_df.iloc[random_index]['Title']
-    random_movie_rating = movies_df.iloc[random_index]['Rating']
-
-    return jsonify({
-        "movie": random_movie,
-        "rating": random_movie_rating
-    })
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('.', path)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
