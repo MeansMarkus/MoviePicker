fe82a66fcc95ca762839033f56acebb5297141ed
Jackson McKay <110795933+jacksonjmckay@users.noreply.github.com>
2025-03-28 04:27:12 -0500

    added more unit tests to test edge cases

--- jackson.py
+++ jackson.py
@@ -18,16 +18,38 @@ def ask_for_age():
 
 # UnitTest using mock UI to automate the manual input that would be required for testing
 class TestAge(unittest.TestCase):
+    # Test for if user is 18/adult
     @patch('tkinter.simpledialog.askinteger', return_value=18)
     def test_adult_age(self, mock_askinteger):
         age = ask_for_age()
         self.assertEqual(age, 18)
         self.assertTrue(age >= 17, "User is an adult.")
 
+    # Test for if user is 16/minor
     @patch('tkinter.simpledialog.askinteger', return_value=16)
     def test_minor_age(self, mock_askinteger):
         age = ask_for_age()
         self.assertEqual(age, 16)
+        self.assertTrue(age < 17, "User is under 17. No R rated movies allowed.")
+
+    # Test for if user did not input anything
+    @patch('tkinter.simpledialog.askinteger', return_value=None)
+    def test_no_input(self, mock_askinteger):
+        age = ask_for_age()
+        self.assertIsNone(age, "No age provided.")
+
+    # Test for if user input 120, the maximum age
+    @patch('tkinter.simpledialog.askinteger', return_value=120)
+    def test_maximum_age(self, mock_askinteger):
+        age = ask_for_age()
+        self.assertEqual(age, 120)
+        self.assertTrue(age >= 17, "User is an adult.")
+    
+    # Test for if user input 0, the minimum age
+    @patch('tkinter.simpledialog.askinteger', return_value=0)
+    def test_maximum_age(self, mock_askinteger):
+        age = ask_for_age()
+        self.assertEqual(age, 0)
         self.assertTrue(age < 17, "User is under 17. No R rated movies allowed.")
 
 if __name__ == "__main__":
